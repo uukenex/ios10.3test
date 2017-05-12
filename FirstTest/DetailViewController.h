69381fc1e0f30a02874d94f9e262ad50a0625293
taehwan jeon <taehwanjeon@taehwanui-MacBook-Air.local>
2017-05-12 18:32:23 +0900

    1

---
+++ FirstTest/DetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  DetailViewController.h
+//  FirstTest
+//
+//  Created by taehwan jeon on 2017. 5. 12..
+//  Copyright © 2017년 taehwan jeon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailViewController : UIViewController
+
+@end
---
+++ FirstTest/DetailViewController.m
@@ -0,0 +1,43 @@
+//
+//  DetailViewController.m
+//  FirstTest
+//
+//  Created by taehwan jeon on 2017. 5. 12..
+//  Copyright © 2017년 taehwan jeon. All rights reserved.
+//
+
+#import "DetailViewController.h"
+
+@interface DetailViewController ()
+
+@end
+
+@implementation DetailViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+
+- (IBAction)buttonClicked:(id)sender {
+    NSLog(@"돌아가!");
+}
+
+
+@end
